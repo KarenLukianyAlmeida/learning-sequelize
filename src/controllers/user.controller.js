58db0461e13e52e3b2842d8241be716954ae556e
Karen Almeida <karenalmeida.lukiany@gmail.com>
2024-06-13 15:49:12 +0100

    Created response of getAll method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,10 +4,14 @@ const { User } = require('../models');
 
 /* Esta função usa o método findAll do Sequelize para buscar todas as linhas da tabela Users
 Equivale a fazer a query: SELECT * FROM Users */
-const getAll = async () => {
-  const users = await User.findAll();
-
-  return users;
+const getAll = async (_req, res) => {
+  try {
+    const users = await userService.getAll();
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
 };
 
 module.exports = {
